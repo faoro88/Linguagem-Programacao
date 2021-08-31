c0e8125eb54b6d56be3b2fd4da7b6653549fadea
faoro88 <faoro.eduardo@gmail.com>
2021-08-30 23:31:33 -0300

    Commit inicial. Classes feitas, porem, sem integração com a tela.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'linguagens-de-prog';
 }
+abstract class Pessoa {
+  palavra: string;
+  numberA: number;
+  numberB: number;
+
+  constructor(palavra: string, numberA: number, numberB: number) {
+    this.palavra = palavra;
+    this.numberA = numberA;
+    this.numberB = numberB;
+  }
+
+  dizerAlgo(): void {
+    console.log(this.palavra);
+  }
+  multiplicar(): void {
+    console.log('Valor Multiplicado=' + this.numberA * this.numberB);
+  }
+  somar(): void {
+    console.log('Valor Somado= ' + (this.numberA + this.numberB));
+  }
+  subtrair(): void {
+    console.log('Valor Subtrair= ' + (this.numberA - this.numberB));
+  }
+  dividir(): void {
+    console.log('Valor Dividir= ' + this.numberA / this.numberB);
+  }
+  calcularRestoDeDivisao(): void {
+    console.log('Valor do Resto da Divisão= ' + (this.numberA % this.numberB));
+  }
+}
+
+class Aluno extends Pessoa {
+  constructor(palavra: string, numberA: number, numberB: number) {
+    super(palavra, numberA, numberB);
+  }
+}
+window.onload = () => {
+  var dizalgo: Aluno = new Aluno(
+    'Estou programando em TypeScript usando Angular',
+    3,
+    2
+  );
+  dizalgo.dizerAlgo();
+  dizalgo.multiplicar();
+  dizalgo.somar();
+  dizalgo.subtrair();
+  dizalgo.dividir();
+  dizalgo.calcularRestoDeDivisao();
+};
